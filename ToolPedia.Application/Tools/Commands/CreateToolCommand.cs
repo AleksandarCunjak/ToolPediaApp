675e903d28ed1e869ae03cc13948b244b01391a8
Aleksandar Cunjak <102021403+AleksandarCunjak@users.noreply.github.com>
2024-05-07 16:24:44 +0200

    Update CreateToolCommand.cs

--- ToolPedia.Application/Tools/Commands/CreateToolCommand.cs
+++ ToolPedia.Application/Tools/Commands/CreateToolCommand.cs
@@ -36,32 +36,12 @@ namespace ToolPedia.Application.Tools.Commands
             )
             {
                 var tool = _mapper.Map<Tool>(request.CreateToolRequest);
-                tool.Images = GenerateSasToken("images", "shaomi.jpg");
 
                 await _dbContext.Tools.AddAsync(tool);
 
                 await _dbContext.SaveChangesAsync(cancellationToken);
 
                 return _mapper.Map<ToolResponse>(tool);
-            }
-
-            public string GenerateSasToken(string containerName, string blobName)
-            {
-                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
-                    "DefaultEndpointsProtocol=https;AccountName=csb1003200368286dfe;AccountKey=1R/NwHio0hIpP3171rF7PFQO8skuepKiulH06VkjWfSBiRqxFBF4gsdrBUGQ7kbScVFRxNH7Htti+AStCwETlA==;EndpointSuffix=core.windows.net"
-                );
-                CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
-                CloudBlobContainer container = blobClient.GetContainerReference(containerName);
-                CloudBlockBlob blob = container.GetBlockBlobReference(blobName);
-
-                SharedAccessBlobPolicy sasPolicy = new SharedAccessBlobPolicy()
-                {
-                    Permissions = SharedAccessBlobPermissions.Read,
-                    SharedAccessExpiryTime = DateTime.UtcNow.AddHours(1), // Set expiry time as needed
-                };
-
-                string sasToken = blob.GetSharedAccessSignature(sasPolicy);
-                return blob.Uri + sasToken;
             }
         }
     }
